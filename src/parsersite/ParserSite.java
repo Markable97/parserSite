72db35aac4d30aaa6299af5affb3abbbc1afe0a9
Mark Glushko <markableglushko@gmail.com>
2018-10-30 13:00:00 +0300

    Парсинг сайта, вытащил из html данные по всем игрокам

--- src/parsersite/ParserSite.java
+++ src/parsersite/ParserSite.java
@@ -5,19 +5,33 @@
  */
 package parsersite;
 
+import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 /**
  *
  * @author Markable
  */
 public class ParserSite {
 
+    static String url = "http://lfl.ru/club2668/players_list"; //ссылка на список игркоов команды 
+    
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        //комментари для 
-        //Git
-        System.out.println("Привет World");
+    public static void main(String[] args) throws IOException {
+        System.out.println("Начало парсинга");
+        Document doc = Jsoup.connect(url).get();
+        Element allListPlayerHtml = doc.select("div.cont.all_news").last();
+        Elements infoPlayer = allListPlayerHtml.getElementsByClass("dark_blue_block");
+        for(Element aboutPlayer : infoPlayer){
+            
+        }
+        System.out.println("size listPlayet = " + infoPlayer.size());
+        
     }
     
 }
