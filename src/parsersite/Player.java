c936ec08fe0a84b91b1c6a81231504da88fdaf47
Mark Glushko <markableglushko@gmail.com>
2018-10-30 14:27:41 +0300

    класс для игрока

---
+++ src/parsersite/Player.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parsersite;
+
+/**
+ *
+ * @author Markable
+ */
+public class Player {
+    
+    String name;
+    String team;
+    String amplua;
+    String birthday;
+    int number;
+
+    public Player(String name, String team, String amplua, String birthday, int number) {
+        this.name = name;
+        this.team = team;
+        this.amplua = amplua;
+        this.birthday = birthday;
+        this.number = number;
+    }
+    
+    
+    
+}
