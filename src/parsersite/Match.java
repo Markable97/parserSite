889f2c5bf985a75a7819fc69ccad60717c10e9ef
Mark Glushko <markableglushko@gmail.com>
2018-11-24 11:46:54 +0300

    Добавил конструкторы (переопределил для сыгранныз и несыгранных матчей). Сеттер на массив игроков

--- src/parsersite/Match.java
+++ src/parsersite/Match.java
@@ -22,5 +22,46 @@ public class Match {
     ArrayList<Player> players;
     String stadium;
     String referee;
+    String matchTransfer;
+
+    public Match(String division, int tour, String dateMatch, String teamHome, int goalHome, int goalGuest, String teamGuest, String stadium, String referee) {
+        //Если матч сыгран
+        this.division = division;
+        this.tour = tour;
+        this.dateMatch = dateMatch;
+        this.teamHome = teamHome;
+        this.goalHome = goalHome;
+        this.goalGuest = goalGuest;
+        this.teamGuest = teamGuest;
+        this.stadium = stadium;
+        this.referee = referee;
+    }
+
+    public Match(String division, int tour, String teamHome, String teamGuest, String matchTransfer) {
+        //Если перенос то указываем команду переноса (Может быть пепенос лиги)
+        this.division = division;
+        this.tour = tour;
+        this.teamHome = teamHome;
+        this.teamGuest = teamGuest;
+        this.matchTransfer = matchTransfer;
+    }
+
+    public void setPlayers(ArrayList<Player> players) {
+        //Копируем пришедший массив в поле класса
+        this.players = new ArrayList<Player>();
+        for(Player e : players){
+            this.players.add(e);
+        }
+        //this.players = players;
+    }
+
+    @Override
+    public String toString() {
+        return "Match{" + "division=" + division + ", tour=" + tour + ", dateMatch=" + dateMatch + 
+                ", teamHome=" + teamHome + ", goalHome=" + goalHome + ", goalGuest=" + goalGuest + 
+                ", teamGuest=" + teamGuest + ",\n players=" + players.toString() + ", stadium=" + stadium + 
+                ", referee=" + referee + ", matchTransfer=" + matchTransfer + "}\n\n";
+    }
+    
     
 }
